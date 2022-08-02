4f8f810dc4d03f40c29e92a3fb7c74f5bb4ea62d
Boyan-Apostolov <58187770+Boyan-Apostolov@users.noreply.github.com>
2022-08-02 18:13:01 +0300

    Add Images API Demo

---
+++ ImagesApiDemo/ImagesApiDemo/Images.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagesApiDemo
+{
+    public class Images
+    {
+        public int Page { get; set; }
+        public int Per_Page { get; set; }
+
+        public ICollection<ImageItem> Photos { get; set; }
+
+        public int Total_Results { get; set; }
+
+        public string Next_Page { get; set; }
+    }
+
+    public class ImageItem
+    {
+        public int Id { get; set; }
+
+        public ImageItemSrc Src { get; set; }
+
+        public string Alt { get; set; }
+    }
+
+    public class ImageItemSrc
+    {
+        public string Original { get; set; }
+    }
+}
---
+++ ImagesApiDemo/ImagesApiDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using ImagesApiDemo;
+using Newtonsoft.Json;
+using System.Net;
+
+Console.Write("Enter keyword to download: ");
+var keyword = Console.ReadLine();
+
+Console.Write("Enter number of images to download: ");
+var numberOfImages = int.Parse(Console.ReadLine());
+
+string API = $"https://api.pexels.com/v1/search/?per_page={numberOfImages}&query={keyword}";
+const string API_KEY = "563492ad6f9170000100000191c82e20b6e64c6a9c8c062728ea86ec";
+
+var images = await GetImagesFromAPI(API, API_KEY);
+
+Console.WriteLine($"Found {images.Per_Page} images...");
+
+var path = Directory.GetCurrentDirectory() + @"\Images";
+Directory.CreateDirectory(path);
+
+foreach (var imageItem in images.Photos)
+{
+    var fileName = imageItem.Alt.Trim().Replace(" ", "_");
+
+    using (WebClient client = new WebClient())
+    {
+        client.DownloadFile(new Uri(imageItem.Src.Original), @$"{path}\{fileName}.jpeg");
+    }
+
+    Console.WriteLine($"Downloaded -> {fileName}");
+}
+
+async Task<Images> GetImagesFromAPI(string API, string API_KEY)
+{
+    HttpClient client = new HttpClient();
+    client.DefaultRequestHeaders.Add("Authorization", API_KEY);
+
+    var page = await client.GetAsync(API);
+
+    string imagesInfoAsString = await page.Content.ReadAsStringAsync();
+
+    var parsedImages = JsonConvert.DeserializeObject<Images>(imagesInfoAsString);
+
+    return parsedImages;
+}
